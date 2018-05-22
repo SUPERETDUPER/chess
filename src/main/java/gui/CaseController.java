422d10aa590a76f3084bec8d9da19e95f8885e99
SUPERETDUPER <machst@live.ca>
2018-05-22 18:18:28 -0400

    Added icon

--- src/main/java/gui/CaseController.java
+++ src/main/java/gui/CaseController.java
@@ -1,17 +1,22 @@
 package gui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 /**
  * Controle une case
  */
 public class CaseController {
+    public static final int HEIGHT_TO_FONT_RATIO = 2;
     @FXML
     private StackPane root;
+    @FXML
+    private Label label;
 
     private final boolean isBlanc;
 
@@ -20,8 +25,9 @@ public class CaseController {
     }
 
     @FXML
-    private void initialize(){
+    private void initialize() {
         //Met la couleur blanc ou gris
-        root.setBackground(new Background(new BackgroundFill(isBlanc ? Color.WHITE : Color.LIGHTGRAY, null,null)));
+        root.setBackground(new Background(new BackgroundFill(isBlanc ? Color.WHITE : Color.LIGHTGRAY, null, null)));
+        root.heightProperty().addListener((observable, oldValue, newValue) -> label.setFont(Font.font(newValue.floatValue() / HEIGHT_TO_FONT_RATIO)));
     }
 }
