ef6e7f670615c66823fe812e74360340e346d1c7
Martin Staadecker <machstg@gmail.com>
2018-05-24 13:38:30 -0400

    Made king extend super class offsetPiece

---
+++ src/main/java/modele/pieces/OffsetPiece.java
@@ -0,0 +1,40 @@
+package modele.pieces;
+
+import modele.board.Board;
+import modele.board.Position;
+import modele.moves.EatMove;
+import modele.moves.Move;
+import modele.moves.NormalMove;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public abstract class OffsetPiece extends Piece {
+    public OffsetPiece(boolean isWhite) {
+        super(isWhite);
+    }
+
+    @Override
+    public Set<Move> generateMoves(Board board) {
+        Set<Move> moves = new HashSet<>();
+
+        Position currentPose = board.getPosition(this);
+
+        for (int[] offset : getOffsets()) {
+            Position nextPosition = currentPose.offset(offset[0], offset[1]);
+
+            //Si la position n'est pas valide passer à la prochaine
+            if (!nextPosition.isValid()) continue;
+
+            Piece pieceAPosition = board.getPiece(nextPosition);
+
+            //si il y a une pièce de la même couleur à cette position, passer à la prochaine
+            if (pieceAPosition == null) moves.add(new NormalMove(currentPose, nextPosition));
+            else if (pieceAPosition.isWhite() != this.isWhite()) moves.add(new EatMove(currentPose, nextPosition));
+        }
+
+        return moves;
+    }
+
+    abstract int[][] getOffsets();
+}
--- src/main/java/modele/pieces/Roi.java
+++ src/main/java/modele/pieces/Roi.java
@@ -1,15 +1,6 @@
 package modele.pieces;
 
-import modele.board.Board;
-import modele.board.Position;
-import modele.moves.EatMove;
-import modele.moves.Move;
-import modele.moves.NormalMove;
-
-import java.util.HashSet;
-import java.util.Set;
-
-public class Roi extends Piece {
+public class Roi extends OffsetPiece {
     private static final int[][] OFFSETS = {
             {-1, -1},
             {-1, 0},
@@ -36,24 +27,7 @@ public class Roi extends Piece {
     }
 
     @Override
-    public Set<Move> generateMoves(Board board) {
-        Set<Move> moves = new HashSet<>();
-
-        Position currentPose = board.getPosition(this);
-
-        for (int[] offset : OFFSETS) {
-            Position nextPosition = currentPose.offset(offset[0], offset[1]);
-
-            //Si la position n'est pas valide passer à la prochaine
-            if (!nextPosition.isValid()) continue;
-
-            Piece pieceAPosition = board.getPiece(nextPosition);
-
-            //si il y a une pièce de la même couleur à cette position, passer à la prochaine
-            if (pieceAPosition == null) moves.add(new NormalMove(currentPose, nextPosition));
-            else if (pieceAPosition.isWhite() != this.isWhite()) moves.add(new EatMove(currentPose, nextPosition));
-        }
-
-        return moves;
+    int[][] getOffsets() {
+        return OFFSETS;
     }
 }
