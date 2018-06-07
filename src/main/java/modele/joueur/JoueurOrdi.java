7348d42b036253fc2c09edaecad99672177c736c
SUPERETDUPER <machst@live.ca>
2018-06-06 21:30:32 -0400

    Fixed algorithm

--- src/main/java/modele/joueur/JoueurOrdi.java
+++ src/main/java/modele/joueur/JoueurOrdi.java
@@ -15,7 +15,7 @@ import java.util.function.Consumer;
  * Un joueur qui utilise un algorithm pour trouver son prochain mouvement
  */
 public class JoueurOrdi extends Joueur {
-    public static final Difficulte NIVEAU_FACILE = new Difficulte(3, 10000, "Facile");
+    public static final Difficulte NIVEAU_FACILE = new Difficulte(3, 20000, "Facile");
     public static final Difficulte NIVEAU_DIFFICILE = new Difficulte(4, 40000, "Difficile");
 
     private final Difficulte difficulte;
@@ -38,116 +38,156 @@ public class JoueurOrdi extends Joueur {
      */
     @Override
     public void getMouvement(Consumer<Mouvement> callback, Couleur couleur) {
-        new Thread(() -> callback.accept(calculerMeilleurMouvement(couleur))).start();
+        new Thread(() -> callback.accept(new Calculateur().calculerMeilleurMouvement(couleur))).start();
     }
 
-    private Mouvement calculerMeilleurMouvement(Couleur couleur) {
-        State bestState = null;
+    private class Calculateur {
+        private int mouvementsAnalyzer = 0;
 
-        int mouvementsAnalyzer = 0;
+        private Mouvement calculerMeilleurMouvement(Couleur couleur) {
+            State bestState = null;
 
-        Queue<State> states = new LinkedList<>(Collections.singletonList(new State(couleur)));
+            Queue<State> states = new LinkedList<>(Collections.singletonList(new State(couleur)));
 
-        while (true) {
-            State state = states.remove(); //Obtenir la prochaine position à analyzer
+            while (true) {
+                State state = states.remove(); //Obtenir la prochaine position à analyzer
 
-            for (Mouvement mouvement : state.getPreviousMoves()) {
-                mouvement.appliquer(jeuData.getPlateau());
-            }
+                for (Mouvement mouvement : state.getPreviousMoves()) {
+                    mouvement.appliquer(jeuData.getPlateau());
+                }
 
-            if (bestState == null) {
-                bestState = state;
-            } else {
-                if (state.getCouleur() == Couleur.NOIR) {
-                    //50% du temps si le mouvement à la même valeur échanger pour avoir de la variété
-                    if ((state.getValue() > bestState.getValue())
-                            || (state.getValue() == bestState.getValue() && Math.random() > 0.5)) {
+                if (state.previousMoves.size() != 0) {
+                    if (bestState == null) {
                         bestState = state;
-                    }
-                } else {
-                    //50% du temps si le mouvement à la même valeur échanger pour avoir de la variété
-                    if (state.getValue() < bestState.getValue()
-                            || (state.getValue() == bestState.getValue() && Math.random() > 0.5)) {
-                        bestState = state;
+                    } else {
+                        if (couleur == Couleur.BLANC) {
+                            //50% du temps si le mouvement à la même valeur échanger pour avoir de la variété
+                            if (state.getValue() > bestState.getValue()
+                                    || (state.getValue() == bestState.getValue() && Math.random() > 0.5)) {
+                                bestState = state;
+                            }
+                        } else {
+                            //50% du temps si le mouvement à la même valeur échanger pour avoir de la variété
+                            if ((state.getValue() < bestState.getValue())
+                                    || (state.getValue() == bestState.getValue() && Math.random() > 0.5)) {
+                                bestState = state;
+                            }
+                        }
                     }
                 }
-            }
 
-            mouvementsAnalyzer += 1;
+                mouvementsAnalyzer += 1;
 
-            System.out.println(state.getSize() + " " + mouvementsAnalyzer);
+                System.out.println(state.getSize() + " " + mouvementsAnalyzer);
 
-            if (state.getSize() >= difficulte.getMinSearchDepth() && mouvementsAnalyzer >= difficulte.getMinSearchMoves()) {
+                if (state.getSize() >= difficulte.getMinSearchDepth() && mouvementsAnalyzer >= difficulte.getMinSearchMoves()) {
+                    for (int i = state.getPreviousMoves().size() - 1; i >= 0; i--) {
+                        state.getPreviousMoves().get(i).undo(jeuData.getPlateau());
+                    }
+
+                    break; //Si passe arreter
+                }
+
+                //Ajouter les autres options à la liste
+                Set<Mouvement> mouvements;
+
+                if (state.getPreviousMoves().size() == 0) {
+                    mouvements = jeuData.getAllLegalMoves(state.getProchainJoueur());
+                } else {
+                    mouvements = jeuData.getAllMoves(state.getProchainJoueur());
+                }
+
+                for (Mouvement mouvement : mouvements) {
+                    System.out.println("analysing: " + mouvement);
+                    mouvement.appliquer(jeuData.getPlateau());
+                    State prochainState = new State(state, mouvement);
+                    states.add(new State(prochainState, getBestMove(prochainState)));
+                    mouvement.undo(jeuData.getPlateau());
+                }
+
                 for (int i = state.getPreviousMoves().size() - 1; i >= 0; i--) {
                     state.getPreviousMoves().get(i).undo(jeuData.getPlateau());
                 }
-
-                break; //Si passe arreter
             }
 
-            //Ajouter les autres options à la liste
+            System.out.println(mouvementsAnalyzer + " " + bestState + " ma couleur " + couleur);
+
+            return bestState.getPreviousMoves().get(0);
+        }
+
+        private Mouvement getBestMove(State state) {
             Set<Mouvement> mouvements;
 
             if (state.getPreviousMoves().size() == 0) {
-                mouvements = jeuData.getAllLegalMoves(state.getCouleur());
+                mouvements = jeuData.getAllLegalMoves(state.getProchainJoueur());
             } else {
-                mouvements = jeuData.getAllMoves(state.getCouleur());
+                mouvements = jeuData.getAllMoves(state.getProchainJoueur());
             }
+
+            Mouvement best = null;
 
             for (Mouvement mouvement : mouvements) {
-                states.add(state.appendAndReturn(mouvement));
+                if (best == null) best = mouvement;
+                else {
+                    if (state.getProchainJoueur() == Couleur.BLANC) {
+                        //50% du temps si le mouvement à la même valeur échanger pour avoir de la variété
+                        if (mouvement.getValeur() > best.getValeur()
+                                || (mouvement.getValeur() == best.getValeur() && Math.random() > 0.5)) {
+                            best = mouvement;
+                        }
+                    } else {
+                        //50% du temps si le mouvement à la même valeur échanger pour avoir de la variété
+                        if ((mouvement.getValeur() < best.getValeur())
+                                || (mouvement.getValeur() == best.getValeur() && Math.random() > 0.5)) {
+                            best = mouvement;
+                        }
+                    }
+                }
+                mouvementsAnalyzer += 1;
             }
 
-            for (int i = state.getPreviousMoves().size() - 1; i >= 0; i--) {
-                state.getPreviousMoves().get(i).undo(jeuData.getPlateau());
+            return best;
+        }
+
+        private class State {
+            private final int value;
+            private final List<Mouvement> previousMoves;
+            private final Couleur prochainJoueur;
+
+            State(State previousState, Mouvement mouvement) {
+                this.value = previousState.value + mouvement.getValeur();
+                this.prochainJoueur = previousState.prochainJoueur == Couleur.BLANC ? Couleur.NOIR : Couleur.BLANC;
+                this.previousMoves = new ArrayList<>(previousState.previousMoves);
+                previousMoves.add(mouvement);
             }
-        }
 
-        System.out.println(mouvementsAnalyzer + " " + bestState.getValue());
+            State(Couleur prochainJoueur) {
+                this.prochainJoueur = prochainJoueur;
+                this.previousMoves = new ArrayList<>();
+                this.value = 0;
+            }
 
-        return bestState.getPreviousMoves().get(0);
-    }
+            List<Mouvement> getPreviousMoves() {
+                return previousMoves;
+            }
 
-    private class State {
-        private final int value;
-        private final List<Mouvement> previousMoves;
-        private final Couleur couleur;
+            int getValue() {
+                return value;
+            }
 
-        State(int value, List<Mouvement> previousMoves, Couleur couleur) {
-            this.value = value;
-            this.couleur = couleur;
-            this.previousMoves = previousMoves;
-        }
+            int getSize() {
+                return previousMoves.size();
+            }
 
-        State(Couleur couleur) {
-            this.couleur = couleur;
-            this.value = 0;
-            this.previousMoves = new ArrayList<>();
-        }
+            Couleur getProchainJoueur() {
+                return prochainJoueur;
+            }
 
-        State appendAndReturn(Mouvement mouvement) {
-            List<Mouvement> newMoveSequence = new ArrayList<>(this.previousMoves);
-            newMoveSequence.add(mouvement);
-            return new State(value + mouvement.getValeur(),
-                    newMoveSequence,
-                    couleur == Couleur.BLANC ? Couleur.NOIR : Couleur.BLANC
-            );
-        }
-
-        List<Mouvement> getPreviousMoves() {
-            return previousMoves;
-        }
-
-        int getValue() {
-            return value;
-        }
-
-        int getSize() {
-            return previousMoves.size();
-        }
-
-        public Couleur getCouleur() {
-            return couleur;
+            @Override
+            public String toString() {
+                return "moves: " + previousMoves +
+                        " value: " + value + " prochain joueur " + prochainJoueur;
+            }
         }
     }
 
