89622dcfe458ec803b2ec552a4cf4fccbe552b04
SUPERETDUPER <machst@live.ca>
2018-06-09 16:28:58 -0400

    Fixed MouvementPromotion

--- src/main/java/modele/moves/MouvementPromotion.java
+++ src/main/java/modele/moves/MouvementPromotion.java
@@ -2,14 +2,28 @@ package modele.moves;
 
 import modele.pieces.Piece;
 import modele.pieces.Reine;
+import modele.plateau.Plateau;
 import modele.plateau.Position;
 
-public class MouvementPromotion extends MouvementNormal {
-    private final Reine reine;
+public class MouvementPromotion extends Mouvement {
+    private Reine reine;
+    private Position debut;
 
-    public MouvementPromotion(Piece piece, Position end, Reine reine) {
+    public MouvementPromotion(Piece piece, Position end) {
         super(piece, end);
-        this.reine = reine;
+    }
+
+    @Override
+    void appliquerInterne(Plateau plateau) {
+        debut = plateau.removePiece(piece);
+        reine = new Reine(piece.getCouleur());
+        plateau.ajouter(fin, reine);
+    }
+
+    @Override
+    void undoInterne(Plateau plateau) {
+        plateau.removePiece(fin);
+        plateau.ajouter(debut, piece);
     }
 
     @Override
